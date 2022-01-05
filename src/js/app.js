eed1cec061954ccb24441738aa2235780835c690
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-01-05 11:46:59 +0100

    légende test

--- src/js/app.js
+++ src/js/app.js
@@ -88,3 +88,24 @@ function carrouselAlternate() {
     });
 }
 
+function monterLegende() {
+    let texte13 = document.querySelector(".texte13");
+    texte13.style.display='block';
+    return anime({
+        targets: texte13,
+        translateY: '-5em',
+        autoplay: false,
+        easing: 'linear',
+        duration: 200,
+    }).play
+}
+
+function descendreLegende() {
+	return anime({
+        targets: '.texte13',
+        translateY: '5em',
+        autoplay: false,
+        easing: 'linear',
+        duration: 200,
+    }).play
+}
